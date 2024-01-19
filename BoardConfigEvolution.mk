9158b5c113256efb88df14dc8db9644c0ca2defc
hurtcopain <jacksummers715@gmail.com>
2024-01-19 15:31:39 -0500

    zuma: Zephyrus

--- BoardConfigEvolution.mk
+++ BoardConfigEvolution.mk
@@ -7,7 +7,7 @@
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/evolution/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/proton/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml
--- device.mk
+++ device.mk
@@ -897,18 +897,6 @@ endif
 
 PRODUCT_PACKAGES += ShannonRcs
 
-ifeq ($(EVOLUTION_BUILD),)
-ifeq (,$(filter aosp_% factory_%,$(TARGET_PRODUCT)))
-#ImsMediaAoc library
-FEATURE_TYPE := oem_audio
-SOONG_CONFIG_NAMESPACES += audio_lib
-SOONG_CONFIG_audio_lib += \
-        audio_type
-
-SOONG_CONFIG_audio_lib_audio_type := $(FEATURE_TYPE)
-endif
-endif
-
 # ImsMedia
 PRODUCT_PACKAGES += \
 	ImsMediaService \
